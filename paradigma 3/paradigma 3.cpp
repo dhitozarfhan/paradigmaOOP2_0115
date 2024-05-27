e4cd418ebb80c1a1f22baf8ff2520de89b581718
dhitozarfhan <dhitozarfhan@gmail.com>
2024-05-27 10:36:29 +0700

    class derivedClass

--- paradigma 3/paradigma 3.cpp
+++ paradigma 3/paradigma 3.cpp
@@ -4,7 +4,20 @@ using namespace std;
 class baseClass {
 public: 
 	virtual void perkenalan() {
-		cout << "Hallo saya Function dari base c;ass";
+		cout << "Hallo saya Function dari base class";
 	}	
 };
 
+class derivedClass : public baseClass {
+public: 
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class"
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
